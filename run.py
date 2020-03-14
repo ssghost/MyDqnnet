3b12cbe4e4339aa59f55ed272b3ec60286d590c5
Shuang Song <6882971+ssghost@users.noreply.github.com>
2020-03-14 22:17:03 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,31 @@
+import getopt, sys
+from dqn.dqnnet import Dqnnet
+from gym_my_maze.gym_my_maze.envs import MyMaze
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'r:c:v:', ['roadpath=','confpath=','videopath='])
+    except getopt.GetoptError as err:
+        print(err) 
+        sys.exit()
+    
+    for o, a in opts:
+        if o in ('-r', '--roadpath') and type(a)==str:
+            roadpath = a
+        elif o in ('-c', '--confpath') and type(a)==str:
+            confpath = a 
+        elif o in ('-v', '--videopath') and type(a)==str:
+            videopath = a
+        else:
+            assert False, 'unhandled option' 
+
+    MyMaze.padding(roadpath)
+    Dqnnet().conf_settings(confpath)
+    Dqnnet().create_env()
+    Dqnnet().create_agent()
+    Dqnnet().create_policy()
+    Dqnnet().train()
+    Dqnnet().create_video(videopath) 
+
+if __name__ == "__main__":
+    main()
